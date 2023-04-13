5609719ca5d24e987d5fcf60a2dbe7fe072cdc28
deborahdelimaa <deborahftlima@gmail.com>
2023-04-13 01:02:56 +0100

    if typed correctly green, if typed wrong red, if not done typing and correct blue

--- 08_02/script.js
+++ 08_02/script.js
@@ -5,13 +5,19 @@ const resetButton = document.querySelector("#reset");
 const theTimer = document.querySelector(".timer");
 
 let timer = [0, 0, 0, 0];
+let interval;
+
 
 // Adiciona zero inicial aos números <= 9 (apenas para estética):
-
+function leadingZero(time){
+    if(time <= 9){
+        time = "0" + time;
+    }
+}
 // Executa um timer padrão de minuto / segundo / centésimos:
 
 function runTimer() {
-  let currentTime = timer[0] + ":" + timer[1] + ":" + timer[2];
+  let currentTime = leadingZero(timer[0]) + ":" + leadingZero(timer[1]) + ":" + timer[2];
   theTimer.innerHTML = currentTime;
   timer[3]++;
   timer[0] = Math.floor(timer[3] / 100 / 60);
@@ -21,16 +27,29 @@ function runTimer() {
 // Verifica se texto digitado com o fornecido na página:
 function spellCheck() {
   let textEntered = testArea.value;
+  let originTextMatch = originText.substring(0, textEntered.length);
+  if(textEntered == originText){
+    testWrapper.style.borderColor = "#429890";
+  }
+  else {
+    if(textEntered== originTextMatch){
+        testWrapper.style.borderColor = "#65CCf3";
+    }  
+   else {
+    testWrapper.style.borderColor = "#E95D0F"
+  }
+
   console.log(textEntered);
+}
 }
 
 // Inicia o cronômetro:
 function start() {
   let textEnteredLength = testArea.value.length;
   if (textEnteredLength === 0) {
-    setInterval(runTimer, 10);
+   interval = setInterval(runTimer, 10);
   }
-  console.log(textEnteredLength);
+ 
 }
 
 // Função de recomeçar:
